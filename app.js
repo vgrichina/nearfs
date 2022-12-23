04b63637b96faf24e70dcb71b82b1be5f8501cc7
Vladimir Grichina <vgrichina@gmail.com>
2022-12-23 09:27:15 +0000

    Make IPFS raw block CIDs work

--- app.js
+++ app.js
@@ -5,32 +5,55 @@ const router = new Router();
 const cors = require('@koa/cors');
 
 const fs = require('fs/promises');
+const multibase = require('multibase');
+const assert = require('assert');
 
 // TODO: Refactor into common module?
 const STORAGE_PATH = process.env.NEARFS_STORAGE_PATH || './storage';
 
 const serveFile = async ctx => {
     // TODO: Cache?
-    const file = `${STORAGE_PATH}/${ctx.params.cid}`;
+    const cidStr = ctx.params.cid;
+    const cid = Buffer.from(multibase.decode(cidStr));
 
-    // TODO: Use params.path and follow IPFS stuff
+    const cidVersion = cid[0];
+    assert(cidVersion === 1, 'Only CID version 1 is supported');
 
-    const stat = await fs.stat(file).catch(() => false);
-    if (stat && stat.isFile()) {
-        // TODO: Detect content-type?
-        ctx.body = await fs.readFile(file);
+    const codec = cid[1];
+    const hashType = cid[2];
+    assert(hashType === 0x12, 'Only SHA-256 is supported');
+    const hashSize = cid[3];
+    assert(hashSize === 32, 'Wrong SHA-256 hash size');
+    const hash = cid.subarray(4, 4 + hashSize);
+
+    if (codec === 0x55) {
+        const file = `${STORAGE_PATH}/${hash.toString('hex')}`;
+
+        assert(ctx.params.path === undefined, 'CID points to a file');
+
+        const stat = await fs.stat(file).catch(() => false);
+        if (stat && stat.isFile()) {
+            // TODO: Detect content-type?
+            ctx.body = await fs.readFile(file);
+            return;
+        }
+    } else if (codec === 0x70) {
+        assert(false, 'Unsupported codec');
     } else {
-        ctx.status = 404;
-        ctx.body = 'Not found';
+        // TODO: Use params.path and follow IPFS stuff
+        throw new Error(`Unsupported codec: 0x${codec.toString(16)}`);
     }
+
+    ctx.status = 404;
+    ctx.body = 'Not found';
 };
 
 router.get('/', async ctx => {
     ctx.body = 'Hello World!';
 });
 
-router.get('/ipfs/:cid', serveFile);
 router.get('/ipfs/:cid/:path', serveFile);
+router.get('/ipfs/:cid', serveFile);
 
 app
     .use(async (ctx, next) => {
