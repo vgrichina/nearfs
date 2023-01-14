29b2c63413d255c68e0dc17407b8ffd86cd97188
Vladimir Grichina <vgrichina@gmail.com>
2023-01-13 19:09:03 -0800

    Use built-in crypto for hash

--- src/util/hash.js
+++ src/util/hash.js
@@ -1,16 +1,9 @@
 async function computeHash(data) {
-    // TODO: Use sync by default as unlikely to be bottleneck?
-    const cryptoAsync = require('@ronomon/crypto-async');
-    const hash = await new Promise((resolve, reject) => {
-        cryptoAsync.hash('sha256', data, (error, hash) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(hash);
-            }
-        });
-    });
-    return hash;
+    // TODO: If becomes bottleneck, try async version @ronomon/crypto-async
+    const crypto = require('crypto');
+    const hash = crypto.createHash('sha256');
+    hash.update(data);
+    return hash.digest();
 }
 
 module.exports = {
