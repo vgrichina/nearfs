374c12900c11e3abf4b5d41c484444d6f6fa68ad
Vladimir Grichina <vgrichina@gmail.com>
2023-01-19 19:50:14 -0800

    Small fix

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const serveFile = async ctx => {
         if (ctx.query.filename) {
             ctx.type = mime.lookup(ctx.query.filename);
             ctx.attachment(ctx.query.filename, { type: 'inline' });
-        } else if (ctx.params.path?.includes('.')) {
+        } else if (path.includes('.')) {
             ctx.type = mime.lookup(path);
         } else {
             const detected = await new Promise((resolve, reject) => magic.detect(fileData, (err, result) => err ? reject(err) : resolve(result)));
